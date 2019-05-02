b36af13de858b6d3c0cf5d36a9c8d8f028409f03
Marilur <rjmarilu@gmail.com>
2019-05-02 21:24:33 +0000

    deployment manager example and update cloudSQL readme file

---
+++ gke-deployment-manager/README.md
@@ -0,0 +1,5 @@
+# Deployment manager for GKE clusters as a code using Deployment Manager
+
+This examples are using Python and it's based on this posting https://medium.com/google-cloud/cloud-deployment-manager-kubernetes-2dd9b8124223
+
+The example gke-cluster-single-pool create a GKE cluster with a single node pool. 
---
+++ gke-deployment-manager/gke-cluster-single-pool/templates/gke-cluster-single-pool.py
@@ -0,0 +1,108 @@
+# This example was generated from this post:
+# https://medium.com/google-cloud/cloud-deployment-manager-kubernetes-2dd9b8124223
+
+def GenerateConfig(context):
+  """Generate YAML resource configuration."""
+
+  cluster_region = context.properties['cluster_region']
+  number_of_nodes = context.properties['num_nodes']
+  name_prefix = context.env['deployment'] + '-' + context.env['name']
+  cluster_name = name_prefix
+  type_name = name_prefix + '-type'
+  k8s_endpoints = {
+      '': 'api/v1',
+      '-apps': 'apis/apps/v1beta1',
+      '-v1beta1-extensions': 'apis/extensions/v1beta1'
+  }
+
+
+  resources = []
+  outputs = []
+
+  resources.append({
+      'name': cluster_name,
+      'type': 'gcp-types/container-v1beta1:projects.locations.clusters',
+      'properties': {
+          'parent': 'projects/{}/locations/{}'.format(context.env['project'], cluster_region),
+          'cluster': {
+              'name': cluster_name,
+              'nodePools': [{
+                  'name': 'core',
+                  'initialNodeCount': number_of_nodes,
+                  'config': {
+                      'imageType': 'COS',
+                      'preemptible': True,
+                      'oauthScopes': [
+                          'https://www.googleapis.com/auth/' + scope
+                          for scope in [
+                              'compute',
+                              'devstorage.read_only',
+                              'logging.write',
+                              'monitoring'
+                          ]
+                      ]
+                  },
+                  'autoscaling': {
+                      'enabled': False
+                  },
+                  'management': {
+                      'autoUpgrade': True,
+                      'autoRepair': True,
+                      'upgradeOptions': {}
+                  }
+              }]
+          }
+      }
+  })
+  for type_suffix, endpoint in k8s_endpoints.iteritems():
+    resources.append({
+        'name': type_name + type_suffix,
+        'type': 'deploymentmanager.v2beta.typeProvider',
+        'properties': {
+            'options': {
+                'validationOptions': {
+                    # Kubernetes API accepts ints, in fields they annotate
+                    # with string. This validation will show as warning
+                    # rather than failure for Deployment Manager.
+                    # https://github.com/kubernetes/kubernetes/issues/2971
+                    'schemaValidation': 'IGNORE_WITH_WARNINGS'
+                },
+                # According to kubernetes spec, the path parameter 'name'
+                # should be the value inside the metadata field
+                # https://github.com/kubernetes/community/blob/master
+                # /contributors/devel/api-conventions.md
+                # This mapping specifies that
+                'inputMappings': [{
+                    'fieldName': 'name',
+                    'location': 'PATH',
+                    'methodMatch': '^(GET|DELETE|PUT)$',
+                    'value': '$.ifNull('
+                             '$.resource.properties.metadata.name, '
+                             '$.resource.name)'
+                }, {
+                    'fieldName': 'metadata.name',
+                    'location': 'BODY',
+                    'methodMatch': '^(PUT|POST)$',
+                    'value': '$.ifNull('
+                             '$.resource.properties.metadata.name, '
+                             '$.resource.name)'
+                }, {
+                    'fieldName': 'Authorization',
+                    'location': 'HEADER',
+                    'value': '$.concat("Bearer ",'
+                             '$.googleOauth2AccessToken())'
+                }]
+            },
+            'descriptorUrl':
+                ''.join([
+                    'https://$(ref.', cluster_name, '.endpoint)/swaggerapi/',
+                    endpoint
+                ])
+        }
+    })
+    outputs.append({
+        'name': 'clusterType' + type_suffix,
+        'value': type_name + type_suffix
+    })
+
+  return {'resources': resources, 'outputs': outputs}
